fca604378950cac92e903552e5e04a1fc0d5f4f1
Greivin7 <greivindca7@hotmail.com>
2016-03-22 13:43:30 -0600

    Registro
    Creacion de registro de usuario

--- correophp/resources/views/register.blade.php
+++ correophp/resources/views/register.blade.php
@@ -26,44 +26,44 @@
 			<div class="derecha col-md-6">
 				<form name="form_registro" method="POST">
 					<header class="pr"><h1>Registro</h1></header> </br>
-						<div class="input-group">
-							<span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
-							<input type="text" class="form-control" placeholder="UserName" aria-describedby="basic-addon1" id="usuario" required>
-						</div>
-				</br>
 					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Email" id="email" aria-describedby="basic-addon2" required>
-						<span class="input-group-addon" id="basic-addon2">@letter.com</span>
+						<span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
+						<input type="text" class="form-control" placeholder="UserName" aria-describedby="basic-addon1" id="usuario" required>
 					</div>
-			</br>
+				</br>
 				<div class="input-group">
-					<span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-lock"></span></span>
-					<input type="password" class="form-control" placeholder="Password" id="contrasena" aria-describedby="basic-addon1" required>
+					<input type="text" class="form-control" placeholder="Email" id="email" aria-describedby="basic-addon2" required>
+					<span class="input-group-addon" id="basic-addon2">@letter.com</span>
 				</div>
-		</br>
+			</br>
 			<div class="input-group">
-				<span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-calendar"></span></span>
-				<input type="date" class="form-control" placeholder="Username" id="calendar" aria-describedby="basic-addon1">
+				<span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-lock"></span></span>
+				<input type="password" class="form-control" placeholder="Password" id="contrasena" aria-describedby="basic-addon1" required>
 			</div>
+		</br>
+		<div class="input-group">
+			<span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-calendar"></span></span>
+			<input type="date" class="form-control" placeholder="Username" id="calendar" aria-describedby="basic-addon1">
+		</div>
 	</br>
 	<select id="sexo" class="tamano">
 		<option  value="Hombre">Hombre</option>
 		<option  value="Mujer">Mujer</option>
 	</select> </br> </br>
-		<p class="text">
-				<input type="checkbox" id="checkterm" name="terminos" value="Terminos"/> 
-				Acepto las Condiciones del Servicio 
-				y la Politica de Privacidad de Letter.
-		</p>
+	<p class="text">
+		<input type="checkbox" id="checkterm" name="terminos" value="Terminos"/> 
+		Acepto las Condiciones del Servicio 
+		y la Politica de Privacidad de Letter.
+	</p>
 </br>
-<button class="boton sombra" target="_blank" onClick="LOGIN.guardarusuario();" >Acceder </button> 
-<button class="boton sombra" type="button" onClick="location.href='index2.html'" >Cancelar </button> </br>
-</form
+<button class="boton sombra" target="_blank" onClick="LOGIN.guardarusuario();">Acceder </button> 
+<button class="boton sombra" type="button" href="{{asset('/login')}}">Cancelar </button> </br>
+</form>
 </div>
 </div>
 </div>
-	<script type="text/javascript" src="js/correo.js"></script>
-	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/correo.js"></script>
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 </html>

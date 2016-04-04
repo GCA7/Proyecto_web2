530cf84b11196f3d577eec35fe7337b1d8dc355b
Greivin7 <greivindca7@hotmail.com>
2016-04-03 20:54:09 -0600

    Creacion de vista
    vista para confirmar correo

---
+++ correophp/resources/views/confirmacion.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.principal')
+@section('content')
+
+<div class="container" style="margin-top: 20%">
+	<h3>Gracias por unirte a letter, por favor confirma tu correo para activar tu cuenta</h3>
+	<input placeholder="email"></input>
+	<button class="boton sombra btn-success-border" type="submit">Cofirmar cuenta</button>
+</div>
+
+
+
+@stop
--- correophp/resources/views/welcome.blade.php
+++ correophp/resources/views/welcome.blade.php
@@ -1,45 +1,74 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layouts.principal')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+@section('content')
+<header class=" color h sombra">
+        <nav>
+            <img class="im verticalLine"src="Imagenes/email-icon.png">
+             <li class="dropdown boton b3">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+            <p class="der2 visible-desktop visible-tablet" id="nomuser"></p>
+            <p class="pe visible-desktop visible-tablet">Letter</p>
+            <p id="username" class="pe" value=""></p>
+        </nav>
+    </header>
+    <section class="container containertan">
+        <div class="nav row">
+            <div class="col-md-12">
+            <button type="button" title="Crear correo nuevo" class="b2" id="redactar" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat">Redactar</button> </br>
+            <hr>
+            <ul >
+                <li id="salida"class="a imagenConPieDeTexto seleccionado"><span class="glyphicon glyphicon-inbox"></span><a class="a left" selected="selected" onClick="LOGIN.cargarcorreos(); LOGIN.seleccionado2();">Salida</a><span class="badge left" id="icorreos"></span></li>
+                <li id="enviados" class="a imagenConPieDeTexto"><span class="glyphicon glyphicon-inbox"></span><a class="a left" onClick="LOGIN.cargarcorreosenviados(); LOGIN.seleccionado(); ">Enviados</a><span class="badge left" id="ienviados"></span></li>
+            </ul>
+         </div>
+        </div>
+        <nav class="floatright">
+            <p id="fecha" class="hoy salto visible-desktop visible-tablet">Hoy</p>
+            <div class="dere">
+                <a value="Actualizar Página" onclick="window.location.reload()"><span title="Recargar la página" class="glyphicon glyphicon-refresh tamano-icon visible-desktop visible-tablet"> </span></a>
+            </div>
+            <hr class="visible-desktop visible-tablet">
+            <div class="row" id="correos_borrados">
+                <div class="col-sm-4 col-md-6">
+                    <form id="correo">
+                </div>
+            </div>
+        </nav>
+    </section>
+        <div class="modal animated fadeInUp arr" keyboard: "false" data-keyboard="false" data-backdrop="static" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form id="emisor" name="form_redactar">
+                            <div class="form-group">
+                                <label for="recipient-name" class="control-label"></label>
+                                <input type="text" class="form-control" id="paramsj" placeholder="Para:">
+                            </div>
+                            <div class="form-group">
+                                <label for="recipient-name" class="control-label"></label>
+                                <input type="text" class="form-control" id="asuntomsj" placeholder="Asunto:">
+                            </div>
+                            <div class="form-group">
+                                <label for="message-text" class="control-label"></label>
+                                <textarea class="form-control" id="contenidomsj" placeholder="Message:"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal" onClick="LOGIN.salidaguardado(); LOGIN.cargarcorreos();">Save Message</button>
+                        <button type="button" class="btn btn-primary" onClick="LOGIN.enviadosguardado(); LOGIN.cargarcorreosenviados();">Send message</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </body>
-</html>
+@endsection

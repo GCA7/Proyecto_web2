329122efe67cf40e0bcd78eabd617d0c1c90153a
Greivin7 <greivindca7@hotmail.com>
2016-04-05 16:04:05 -0600

    Validacion
    Validacion al momento de loguearse

--- correophp/app/Http/Controllers/UsersController.php
+++ correophp/app/Http/Controllers/UsersController.php
@@ -14,84 +14,78 @@ class UsersController extends Controller
 {
     //
 
-public function create()
-{
-    return view('register');
-}
+    public function create()
+    {
+        return view('register');
+    }
 
-public function insertar(UserRequest $request)
-{
-    echo $request->input('username');
-    if($this->validacion($request->input('email'))){
-    echo "funko";
-    $user = new User;
-    $user->username = $request->input('username');
-    $user->email = $request->input('email');
-    $user->password = Crypt::encrypt($request->input('password'));
-    $user->save();
-    $this->email($request->input('email')); 
-    return view('login');
-}
-    
-}   
-
-public function validacion($mail)
-{
-    $results = DB::table('users')->get();
-
-    foreach ($results as $value) {
-        if($value->email == $mail)
-        {
-            echo "no funko :(";
-            return false;
-        }else{
+    public function insertar(UserRequest $request)
+    {
+        echo $request->input('username');
+        if($this->validacion($request->input('email'))){
             echo "funko";
-            return true;
+            $user = new User;
+            $user->username = $request->input('username');
+            $user->email = $request->input('email');
+            $user->password = Crypt::encrypt($request->input('password'));
+            $user->save();
+            $this->email($request->input('email')); 
+            return redirect('/login');
         }
         
-    }
-}
+    }   
 
-public function email($correo)
-{
+    public function validacion($mail)
+    {
+        $results = DB::table('users')->get();
+
+        foreach ($results as $value) {
+            if($value->email == $mail)
+            {
+                return false;
+            }else{
+                return true;
+            }
+            
+        }
+    }
+
+    public function email($correo)
+    {
    #$data=[];
    #Mail::send('register', [ 'user'  =>  $data ], function ($msj) {
           # $msj->from('letterinfo@letter.com', 'Letter Inc');
 
            #$msj->to($correo)->subject('Favor ingrese al siguiente link para confirmar tu cuenta');
             #echo "Si funko";
-    Mail($correo,'funke','correo','greivindca7@gmail.com');
+        Mail($correo,'funke','correo','greivindca7@gmail.com');
 #});
-}
-
-public function validarlogin(Request $request)
-{
-    $email = $request->input('log_email');
-    $password = $request->input('log_pass');
-    $results = DB::table('users')->get();
-    #$user= DB::table('users')->select('password')->where('email',$email)->where('estado','false')->get();
-    foreach ($results as $value) {
-        $passdecrypt= Crypt::decrypt($value->password);
-        if($value->email == $email && $passdecrypt == $password){
-            echo "si logueo";
-            return view('correoprincipal');
-        }else{
-            echo "no logueo";
-        }
     }
-}
 
-public function Login(Request $request){
-    $user= DB::table('users')->select('password')->where('email',$request->input("log_email"))->where('estado','false')->get();
+   public function Login(Request $request){
+    $user= DB::table('users')->select('password', 'email', 'estado')->where('email',$request->input("log_email"))->get();
     foreach ($user as $user) {
       $contraseña= Crypt::decrypt($user->password);
-      if ($contraseña==$request->input("log_pass")) {
-        #Cache::add('user',$request->input("log_email"),60);
-            return view('correoprincipal');
+      if ($user->estado==0){
+        echo "No has activado tu cuenta";
+      }else{
+      if ($contraseña == $request->input("log_pass") && $user->email == $request->input("log_email") ){
+        #Cache::add('user',$request->input("email"),60);
+        echo "funcko";
+            return redirect('/correoprincipal');
+      }else{
+        echo "usuario o contraseña incorrecta";
       }
     }
+    }
   }
- 
+
+    public function activaruser(Request $request)
+    { 
+    DB::table('users')->where('email', $request->input('email'))->update(['estado' => true]);
+        return redirect('/loguear'); 
+}
+
 }
 
 
